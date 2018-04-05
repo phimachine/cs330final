57e32e31ac32f474422eb0a45023f0d18a7a7ac9
Jason Hu <huyaojie168@me.com>
2018-04-05 11:40:35 -0500

    adding skeleton

---
+++ js/facebook.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function tryApi(){
+    console.log("accessing a random Facebook user")
+    let config={}
+    config.method="GET"
+    config.header={'Content-Type':'application/json','Accept':'application/json'}
+
+    let get_string="graph.facebook.com/v2.12/10000010"
+
+    let answer
+
+
+    
+}
