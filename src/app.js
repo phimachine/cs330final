6200450822650fac8600ec6ba917cb13cbda5a4e
Jason Hu <huyaojie168@me.com>
2018-04-12 21:54:56 -0500

    merged google. updating

--- src/app.js
+++ src/app.js
@@ -143,7 +143,7 @@ function initMap() {
     };
   });
 
-  const apiKey = 'YOUR_API_KEY';
+  const apiKey = 'AIzaSyAsidkmuLavmoPyURZMxIyBZ0_lB4XqUAI';
   const infoWindow = new google.maps.InfoWindow();
   infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
 
--- src/fserver.py
+++ src/fserver.py
@@ -16,14 +16,22 @@ def send_css(path):
 def root():
     return send_from_directory('',"index.html")
 
+@app.route('/<path:path>')
+def send_root(path):
+    return send_from_directory('',path)
+
+@app.route('/img/<path:path>')
+def send_img(path):
+    return send_from_directory("img",path)
+
 @app.route('/googlemaps')
 def googlemaps():
     return send_from_directory("","googlemaps.html")
 
-@app.route('/src/<path:path>')
+@app.route('/<path:path>')
 def sends_src(path):
     return send_from_directory("src",path)
 
 
 if __name__=="__main__":
-    app.run(debug=True, port=5001, ssl_context=('cert/server.crt','cert/server.key'))
+    app.run(debug=True, port=5001, ssl_context=('../cert/server.crt','../cert/server.key'))
