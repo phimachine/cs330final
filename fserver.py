964a2aaca154498f7392d9261da65a4ee7be8c1a
Hung Tran <tranhu01@luther.edu>
2018-05-15 17:47:56 -0500

    User Login and Registration

--- fserver.py
+++ fserver.py
@@ -1,20 +1,34 @@
 from flask_cors import CORS
 from yelp_query import *
-from flask import Flask, render_template, send_from_directory, Response, request
+from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g, send_from_directory
+from flask.ext.login import login_user , logout_user , current_user , login_required
+from flask.ext.login import LoginManager
 from flask_wtf import Form
 from wtforms import SelectField, DecimalField, BooleanField, SubmitField, StringField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app=Flask(__name__,static_url_path="")
+
 Bootstrap(app)
 app.debug=True
 app.secret_key = 'jasonhu'
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 
 class SearchForm(Form):
     location = StringField(label="Location",render_kw={'autofocus': True, 'required':True , 'placeholder':'Location'})
     term = StringField(label="Search Term",render_kw={"required":True, 'placeholder':'Search Term'})
     submit =SubmitField("Find some restaurants")
+
 
 class LoginForm(Form):
     email = StringField(label="Email",render_kw={"type":"email", 'autofocus': True, 'required':True , 'placeholder':'Location'})
@@ -78,6 +92,78 @@ def send_css(path):
 @app.route('/img/<path:path>')
 def send_img(path):
     return send_from_directory("img",path)
+
+#------------------------------------------
+# Register & Login with flask-login
+#------------------------------------------
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(10))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'], request.form['password'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(username=username,password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 if __name__=="__main__":
     app.run(debug=True, port=5001, ssl_context=('cert/server.crt','cert/server.key'))
