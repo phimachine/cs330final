2d359e1b3bf7b930b759e0e55b304013fc891c11
Hung Tran <tranhu01@luther.edu>
2018-05-15 20:26:54 -0500

    Users Login Register

--- fserver.py
+++ fserver.py
@@ -1,8 +1,6 @@
 from flask_cors import CORS
 from yelp_query import *
-from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g, send_from_directory
-from flask_login import login_user , logout_user , current_user , login_required
-from flask_login import LoginManager
+from flask import Flask,session, request, flash, url_for, redirect, render_template, abort ,g, send_from_directory, Response
 from flask_wtf import Form
 from wtforms import SelectField, DecimalField, BooleanField, SubmitField, StringField
 from wtforms.validators import DataRequired
@@ -17,11 +15,9 @@ Bootstrap(app)
 app.debug=True
 app.secret_key = 'jasonhu'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+db.create_all()
 
 
 class SearchForm(Form):
@@ -97,73 +93,54 @@ def send_img(path):
 # Register & Login with flask-login
 #------------------------------------------
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column('user_id', db.Integer, primary_key=True)
-    username = db.Column('username', db.String(20), unique=True, index=True)
-    password = db.Column('password', db.String(10))
-    email = db.Column('email', db.String(50), unique=True, index=True)
-    registered_on = db.Column('registered_on', db.DateTime)
 
-    def __init__(self, username, password, email):
+class User(db.Model):
+    """ Create user table"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.email = email
-        self.registered_on = datetime.utcnow()
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return unicode(self.id)
-
-    def __repr__(self):
-        return '<User %r>' % (self.username)
 
 
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Login Form"""
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form['username']
+        passw = request.form['password']
+        try:
+            data = User.query.filter_by(username=name, password=passw).first()
+            if data is not None:
+                session['logged_in'] = True
+                return redirect(url_for('home'))
+            else:
+                return 'Dont Login'
+        except:
+            return "Dont Login"
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    user = User(request.form['username'], request.form['password'], request.form['email'])
-    db.session.add(user)
-    db.session.commit()
-    flash('User successfully registered')
-    return redirect(url_for('login'))
-
-@app.route('/login',methods=['GET','POST'])
-def login():
-    if request.method == 'GET':
+    """Register Form"""
+    if request.method == 'POST':
+        new_user = User(username=request.form['username'], password=request.form['password'])
+        db.session.add(new_user)
+        db.session.commit()
         return render_template('login.html')
-    username = request.form['username']
-    password = request.form['password']
-    registered_user = User.query.filter_by(username=username,password=password).first()
-    if registered_user is None:
-        flash('Username or Password is invalid' , 'error')
-        return redirect(url_for('login'))
-    login_user(registered_user)
-    flash('Logged in successfully')
-    return redirect(request.args.get('next') or url_for('index'))
+    return render_template('register.html')
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
-    logout_user()
-    return redirect(url_for('index'))
+    """Logout Form"""
+    session['logged_in'] = False
+    return redirect(url_for('home'))
 
-@app.before_request
-def before_request():
-    g.user = current_user
 
 if __name__=="__main__":
     app.run(debug=True, port=5001, ssl_context=('cert/server.crt','cert/server.key'))
